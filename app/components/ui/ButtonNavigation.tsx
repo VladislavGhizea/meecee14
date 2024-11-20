119cbe1e0dacfc3e50f30a42f65b6120e222e732
Vladislav <vladislavghizea666@gmail.com>
2024-11-20 10:50:09 +0000

    fixing home page

--- app/components/ui/ButtonNavigation.tsx
+++ app/components/ui/ButtonNavigation.tsx
@@ -4,17 +4,20 @@ interface ButtonNavigationProps {
   children: React.ReactNode;
   category: string;
   className?: string;
+  isBefore?: boolean;
 }
 
 const ButtonNavigation: React.FC<ButtonNavigationProps> = ({
   children,
   category,
   className,
+  isBefore,
 }) => {
   return (
-    <div>
-      {children}
-      <h2 className={` font-bright-melody ${className} `}>{category}</h2>
+    <div className="flex flex-row">
+      {isBefore ? children : null}
+      <h2 className={`  font-bright-melody ${className} `}>{category}</h2>
+      {isBefore ? null : children}
     </div>
   );
 };
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,25 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import ButtonAction from "../components/ui/ButtonAction";
 import ButtonNavigation from "../components/ui/ButtonNavigation";
+import { useRouter } from "next/navigation";
 export default function Page() {
+  const router = useRouter();
   return (
     <div>
       <div className="header flex flex-row ml-5">
         <Image alt="" src={"/brand.svg"} width={220} height={88} />
         <div className="w-16 h-16 rounded-full bg-rosso70 grid justify-center content-center ml-auto mr-2 relative top-2 mb-4">
-          <Image alt="" src={"/logo.png"} width={56} height={56} />
+          <Image
+            alt=""
+            src={"/logo.png"}
+            width={56}
+            height={56}
+            onClick={() => {
+              router.push("/user");
+            }}
+          />
         </div>
       </div>
       <div className=" bg-verde flex justify-center content-center ml-auto mr-auto w-[20.5rem] h-[30rem] rounded-3xl">
@@ -26,18 +37,32 @@ export default function Page() {
           <Image alt="" src={"star.svg"} width={40} height={40} />
         </ButtonAction>
       </div>
-      <div className="grid grid-flow-col grid-cols-2 grid-rows-1 justify-items-center items-center mb-3">
-        <ButtonAction className=" w-[5.5rem] h-[5.5rem] border-8">
+      <div className="grid grid-flow-col grid-cols-2 grid-rows-1 justify-items-center items-center mb-3 mx-[5rem]">
+        <ButtonAction className=" w-[5.5rem] h-[5.5rem] border-[6px]">
           <Image alt="" src={"cancel.svg"} width={56} height={56} />
         </ButtonAction>
-        <ButtonAction className=" w-[5.5rem] h-[5.5rem] border-8">
+        <ButtonAction className=" w-[5.5rem] h-[5.5rem] border-[6px]">
           <Image alt="" src={"heart.svg"} width={64} height={64} />
         </ButtonAction>
       </div>
-      <div className="navbar flex flex-row bg-rosso70 mx-5 rounded">
-        <ButtonNavigation category="home"> meow </ButtonNavigation>
-        <ButtonNavigation className=" ml-auto" category="match">
-          meow
+      <div className="navbar h-16 w-[23rem] flex flex-row content-center justify-between bg-rosso70 mx-5 rounded-full">
+        <ButtonNavigation isBefore category="home">
+          <Image
+            alt=""
+            src={"/paws.png"}
+            width={48}
+            height={48}
+            className=" w-12 h-12"
+          />
+        </ButtonNavigation>
+        <ButtonNavigation className=" ml-auto " category="match">
+          <Image
+            alt=""
+            src={"/carrier.png"}
+            width={48}
+            height={48}
+            className=" w-12 h-12"
+          />
         </ButtonNavigation>
       </div>
     </div>
